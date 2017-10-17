fe8ff506108b6a052c4f1b97b912805770e6b064
adnanel <adnanel94@gmail.com>
2017-10-17 10:21:28 +0200

    rom parse utils

--- core/RomReader.cpp
+++ core/RomReader.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "RomReader.h"
+
+
+std::ostream& operator<<(std::ostream& stream, CartidgeType type) {
+    switch ( type ) {
+        case RomOnly:
+            stream<<"ROM ONLY";
+            break;
+        case RomMbc1:
+            stream<<"ROM + MBC1";
+            break;
+        case RomMbc1Ram:
+            stream<<"ROM + MBC1 + RAM";
+            break;
+        case RomMbc1RamBattery:
+            stream<<"ROM + MBC1 + RAM + BATTERY";
+            break;
+        case RomMbc2:
+            stream<<"ROM + MBC2";
+            break;
+        case RomMbc2Battery:
+            stream<<"ROM + MBC";
+            break;
+        default:
+            stream<<"N/A";
+            break;
+    }
+
+    return stream;
+}
+
+
+std::ostream& operator<<(std::ostream& stream, CartidgeLanguage lang) {
+    switch ( lang ) {
+        case Japanese:
+            stream<<"Japanese";
+            break;
+        case English:
+            stream<<"English";
+            break;
+        default:
+            stream<<"N/A";
+    }
+    return stream;
+}
+
--- core/RomReader.h
+++ core/RomReader.h
@@ -13,47 +13,26 @@
 https://fms.komkon.org/GameBoy/Tech/Software.html
 
 The internal information area is located at 0100-014F in each cartridge. It contains following values:
-0100-0103  A sequence of bytes 00 C3 xx xx where last two bytes contain the
-           starting address of a cartridge (lower byte first). The first two
-           bytes of this sequence can be used as a "magic number" to
-           recognize GameBoy cartridges. When GameBoy starts, the control is
-           passed to address 0100 and then the sequence is interpreted as
-           NOP; JP .
 0105-0133  Nintendo character area:
            CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D
            00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99
            BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E
-0134-0143  Title of the game in ASCII terminated by zeroes
 0144-0145  Extended manufacturer code in ASCII, when 33 stored at 014B
 0146       Not used
-0147       Cartridge type:
-           0 - ROM ONLY         3 - ROM+MBC1+RAM+BATTERY
-           1 - ROM+MBC1         5 - ROM+MBC2
-           2 - ROM+MBC1+RAM     6 - ROM+MBC2+BATTERY
-0148       ROM size:
-           0 - 256kBit =  32kB =  2 banks
-           1 - 512kBit =  64kB =  4 banks
-           2 -   1MBit = 128kB =  8 banks
-           3 -   2MBit = 256kB = 16 banks
-           4 -   4MBit = 512kB = 32 banks
-0149       RAM size:
-           0 - None
-           1 -  16kBit =  2kB = 1 bank
-           2 -  64kBit =  8kB = 1 bank
-           3 - 256kBit = 32kB = 4 banks
-014A       Language:
-           0 - Japanese game
-           1 - English game
 014B       Manufacturer code:
            33 - Nintendo or extended (see 0144-0145)
            79 - Accolade
            A4 - Konami
-014C       Version number
 014D       Complement check
 014E-014F  Checksum (higher byte first) produced by adding all bytes of
            a cartridge except for two checksum bytes together and taking
            two lower bytes of the result.
  */
+
+enum CartidgeLanguage {
+    Japanese = 0,
+    English = 1
+};
 
 enum CartidgeType {
     RomOnly = 0,
@@ -64,11 +43,31 @@ enum CartidgeType {
     RomMbc2Battery = 6
 };
 
+std::ostream& operator<<(std::ostream& stream, CartidgeType type);
+
+std::ostream& operator<<(std::ostream& stream, CartidgeLanguage lang);
+
 class RomReader {
     std::ifstream mRomFile;
 
 public:
+    CartidgeLanguage getCartidgeLanguage() {
+        /**
+cart 014A
+Language:
+           0 - Japanese game
+           1 - English game
+         */
+
+        mRomFile.seekg(0x14A);
+        return static_cast<CartidgeLanguage>(mRomFile.get());
+    }
+
     std::string getGameTitle() {
+        /**
+address 0134-0143  Title of the game in ASCII terminated by zeroes
+
+         */
         mRomFile.seekg(0x134);
         const int length = 0x143 - 0x0134;
         char str[length];
@@ -78,11 +77,40 @@ public:
     }
 
     CartidgeType getCartidgeType() {
+/**
+address 0147
+Cartridge type:
+           0 - ROM ONLY         3 - ROM+MBC1+RAM+BATTERY
+           1 - ROM+MBC1         5 - ROM+MBC2
+           2 - ROM+MBC1+RAM     6 - ROM+MBC2+BATTERY
+
+ */
         mRomFile.seekg(0x0147);
         return static_cast<CartidgeType>(mRomFile.get());
     }
 
+    int getCartidgeVersion() {
+    /**
+address 014C
+     Version number
+     */
+
+        mRomFile.seekg(0x14c);
+
+        return mRomFile.get();
+    }
+
     unsigned long long getCartStartAddress() {
+        /**
+address 0100-0103
+        A sequence of bytes 00 C3 xx xx where last two bytes contain the
+        starting address of a cartridge (lower byte first). The first two
+        bytes of this sequence can be used as a "magic number" to
+        recognize GameBoy cartridges. When GameBoy starts, the control is
+        passed to address 0100 and then the sequence is interpreted as
+        NOP; JP .
+
+         */
         mRomFile.seekg(0x100);
 
         if ( mRomFile.get() != 0 ) throw std::invalid_argument("Invalid file!");
@@ -94,7 +122,37 @@ public:
         return (static_cast<unsigned long long>(high) << 8) | low;
     }
 
+    unsigned long long getRamSize() {
+        /**
+Address 0149       RAM size:
+           0 - None
+           1 -  16kBit =  2kB = 1 bank
+           2 -  64kBit =  8kB = 1 bank
+           3 - 256kBit = 32kB = 4 banks
+
+         */
+        mRomFile.seekg(0x149);
+        switch (mRomFile.get()) {
+            case 0: return 0;
+            case 1: return 2 * 1024;
+            case 2: return 8 * 1024;
+            case 3: return 32 * 1024;
+            default:
+                throw std::invalid_argument("Invalid ram size!");
+        }
+    }
+
     unsigned long long getRomSize() {
+/**
+address 0148
+ROM size:
+           0 - 256kBit =  32kB =  2 banks
+           1 - 512kBit =  64kB =  4 banks
+           2 -   1MBit = 128kB =  8 banks
+           3 -   2MBit = 256kB = 16 banks
+           4 -   4MBit = 512kB = 32 banks
+
+ */
         if ( 1 < 2 ) return 100;
 
         mRomFile.seekg(0x148);
